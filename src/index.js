5f93bc72e531bd3423daed7a5747d7e8d4bb2cb0
haodinh114 <minhhao114@gmail.com>
2021-01-16 21:41:13 -0800

    slider

--- src/index.js
+++ src/index.js
@@ -7,10 +7,55 @@ import 'bootstrap/dist/css/bootstrap.css'
 // import Counter from './components/counter';
 // import Card from "./components/cardDesign";
 import Card from "./components/cards"
+import './App.css';
+import Slider from 'rc-slider';
+import "rc-slider/assets/index.css";
+
+const mark = [
+{
+value:0,
+label:"$50"
+},
+{
+value:50,
+label:"$75"
+},
+{
+value:100,
+label:"$100"
+},
+{
+value:150,
+label:"$125"
+},
+{
+value:200,
+label:"$150"
+}
+]
+
+const getValue =(e, val) => {
+console.warn(val)
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <Card />
+
+     <div style = {{width:500, margin:40}}>
+     <p style= {{marginBottom: "4cm"}}>
+         Choose your budget
+     </p>
+      <Slider
+        defaultValue = {50}
+        max = {4}
+        step = {null}
+        marks = {mark}
+        valueLabelDisplay="auto"
+        onCharge={getValue}
+      />
+     </div>
+
+  <Card />
   </React.StrictMode>,
   document.getElementById('root')
 );
