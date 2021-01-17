265890e916175129bb7578bff00414bdf6124a21
Ninjasaws <shcherbinadavid13@gmail.com>
2021-01-16 21:16:03 -0800

    1.2 	Addressable Text Webpage Links 		(redirects to other part of website once clicked on) 	Removed logo
    - Needs Vertical Alignment fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
-import App  from '../App.css';
+import Header from "./common/header";
+import './common/header/Header.css';
+
 
 function About(){
     return(
       <div>
+          <section className="header-top__navigation">
+              <Header />
+          </section>
           <h1>About Page</h1>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './App.css';
+import About from "./components/About";
+
 import './index.css';
-// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 // import Counter from './components/counter';
 // import Card from "./components/cardDesign";
 import Card from "./components/cards"
 import Header from './components/common/header/Header';
-import About from "./components/About";
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+const Home = () => (
+    <div>
+        <Header/>
+        <Card />
+    </div>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-      <Header />
-      <Card />
+      <Router>
+          <Switch>
+            <Route path={"/about-us"} component={About}/>
+            <Route path={"/"} component={Home}/>
+          </Switch>
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
